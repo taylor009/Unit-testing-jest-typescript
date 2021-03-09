7eae5eafb97c2cd401cb0d4c3f59e44f33ff71f8
Taylor <taylor@baremetalcloud.com>
2021-03-09 03:22:45 -0500

    Created mock for return values from login request

--- src/test/Handlers/LoginHandler.test.ts
+++ src/test/Handlers/LoginHandler.test.ts
@@ -1,5 +1,6 @@
 import { LoginHandler } from "../../app/Handlers/LoginHandler";
-import {HTTP_CODES, HTTP_METHODS} from "../../app/Models/ServerModels";
+import {HTTP_CODES, HTTP_METHODS, SessionToken} from "../../app/Models/ServerModels";
+import {Utils} from "../../app/Utils/Utils";
 
 
 describe('LoginHandler test suite', () => {
@@ -9,9 +10,15 @@ describe('LoginHandler test suite', () => {
         method: ''
     };
     const responseMock = {
-        writeHead: jest.fn()
+        writeHead: jest.fn(),
+        write: jest.fn(),
+        statusCode: 0
     };
-    const authorizerMock = {};
+    const authorizerMock = {
+        generateToken: jest.fn()
+    };
+
+    const getRequestBodyMock = jest.fn();
 
     beforeEach(() => {
        loginHandler = new LoginHandler(
@@ -19,11 +26,20 @@ describe('LoginHandler test suite', () => {
            responseMock as any,
            authorizerMock as any,
        );
+       Utils.getRequestBody = getRequestBodyMock;
     });
 
     afterEach(() => {
         jest.clearAllMocks();
     });
+
+    const someSessionToken:SessionToken = {
+      tokenId: 'someTokenId',
+      userName: 'someUserName',
+      valid: true,
+      expirationTime: new Date(),
+      accessRights: [1, 2, 3]
+    };
 
     test('options request', async () => {
         requestMock.method = HTTP_METHODS.OPTIONS;
@@ -35,5 +51,18 @@ describe('LoginHandler test suite', () => {
         requestMock.method = 'someRandomMethod';
         await loginHandler.handleRequest();
         expect(responseMock.writeHead).not.toHaveBeenCalled();
+    });
+
+    test.only('post request with valid login', async () => {
+        requestMock.method = HTTP_METHODS.POST;
+        getRequestBodyMock.mockReturnValueOnce({
+            username: 'someUser',
+            password: 'password'
+        });
+        authorizerMock.generateToken.mockReturnValueOnce(someSessionToken);
+        await loginHandler.handleRequest();
+        expect(responseMock.statusCode).toBe(HTTP_CODES.CREATED);
+        expect(responseMock.writeHead).toBeCalledWith(HTTP_CODES.CREATED, { 'Content-Type': 'application/json' });
+        expect(responseMock.write).toHaveBeenCalledWith(JSON.stringify(someSessionToken))
     });
 });
